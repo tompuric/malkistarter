9f6c4cf7f282924957452ef7ca2ec25ebf118e40
Michael Malki <mmalki12.mm@gmail.com>
2014-01-21 17:14:46 +1100

    added comment

--- src/malic/com/example/tutorial_1/Test.java
+++ src/malic/com/example/tutorial_1/Test.java
@@ -17,7 +17,7 @@ public class Test extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.test, menu);
-        // test
+        // tes
         
         return true;
     }
